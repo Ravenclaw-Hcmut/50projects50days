4fa7aa0e1fac9e60b328fc94ebe9fca8e3d53615
Luan Huynh <luanhuynh2001@gmail.com>
2022-03-16 15:45:03 +0700

    init day2

---
+++ script.js
@@ -0,0 +1,16 @@
+// const panels = document.querySelectorAll('.panel');
+
+// panels.forEach(element => {
+//     element.addEventListener('click', () => {
+//         removeActiveClass()
+//         element.classList.add('active')
+        
+//     })
+// });
+
+// function removeActiveClass() {
+//     panels.forEach(element => {
+//         element.classList.remove('active')
+//     }
+//     )
+// }
